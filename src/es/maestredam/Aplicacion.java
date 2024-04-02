cd6137eebf4d781cf33bc12081cc0e3f74948e1e
jesus <javelascot01@educastillalamancha.es>
2024-04-02 12:54:17 +0200

    Añadido clase FUtil y creado aplicacion de prueba

---
+++ src/es/maestredam/Aplicacion.java
@@ -0,0 +1,9 @@
+package es.maestredam;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println(FUtil.existe("archivo1"));
+        System.out.println(FUtil.existe("archivo2"));
+        System.out.println(FUtil.existe("archivo3"));
+    }
+}
