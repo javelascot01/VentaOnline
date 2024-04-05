22feeb16c05c6f9b0dff26a395aff633a4339d10
jesus <javelascot01@educastillalamancha.es>
2024-04-05 12:36:24 +0200

    Documentado metodo existe() para javadoc

--- src/es/maestredam/FUtil.java
+++ src/es/maestredam/FUtil.java
@@ -2,6 +2,12 @@ package es.maestredam;
 
 import java.io.File;
 public class FUtil {
+    /**
+     *  Método que comprueba si existe un archivo con el nombre pasado
+     * @param filename String que se desea saber si se encuentra en el sistema
+     * @return True si existe false si no
+     * @throws IllegalArgumentException lanza excepcion al pasarle el parametro vacio o con espacios
+     */
     public static boolean existe(String filename)throws IllegalArgumentException {
         if (filename.isBlank()){
             throw new IllegalArgumentException("La cadena no puede estar vacia");
