c2ad7c6566aed25a09a6465b94badb16f091c717
jesus <javelascot01@educastillalamancha.es>
2024-04-05 12:27:00 +0200

    Añadido cambios aplicacion y metodo existe

--- src/es/maestredam/Aplicacion.java
+++ src/es/maestredam/Aplicacion.java
@@ -2,8 +2,6 @@ package es.maestredam;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println(FUtil.existe("archivo1"));
-        System.out.println(FUtil.existe("archivo2"));
-        System.out.println(FUtil.existe("archivo3"));
+        FUtil.existe("");
     }
 }
--- src/es/maestredam/FUtil.java
+++ src/es/maestredam/FUtil.java
@@ -2,7 +2,10 @@ package es.maestredam;
 
 import java.io.File;
 public class FUtil {
-    public static boolean existe(String filename) {
+    public static boolean existe(String filename)throws IllegalArgumentException {
+        if (filename.isBlank()){
+            throw new IllegalArgumentException("La cadena no puede estar vacia");
+        }
         File file = new File(filename);
         if (file.exists()) {
             return true;
